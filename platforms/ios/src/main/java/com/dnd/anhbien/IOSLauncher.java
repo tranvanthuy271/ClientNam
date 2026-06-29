5040c925424105f20a4c4e56d4c4b5551bee67e2
ndwcdy <ndwcdy@192.168.145.128>
2026-06-29 10:34:04 -0700

    fix build project

--- platforms/ios/src/main/java/com/dnd/anhbien/IOSLauncher.java
+++ platforms/ios/src/main/java/com/dnd/anhbien/IOSLauncher.java
@@ -1,26 +1,30 @@
-package com.beatdz.langlalau.ios;
+package com.dnd.anhbien;
 
 import beatdz.AppListener;
 import beatdz.Binary;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
 import com.boot.GameM;
 import com.platform.MobilePlatformAdapter;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
-/** Launches the shared game client on iOS through RoboVM. */
+/** Launches the shared game client on iOS. */
 public final class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         Binary.a = "animesoft/1/";
+
         MobilePlatformAdapter platform = new MobilePlatformAdapter();
         GameM.b(platform);
         GameM.a(platform);
 
         IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
-        configuration.orientationLandscape = true;
-        configuration.orientationPortrait = false;
+        configuration.useAccelerometer = false;
+        configuration.useCompass = false;
+        configuration.hdpiMode = HdpiMode.Pixels;
+
         return new IOSApplication(AppListener.gI(), configuration);
     }
 
